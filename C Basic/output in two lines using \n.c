886c7eab00dff77807fd7144ddb8862fbe57807b
nandavardhan05-boop <nandavardhan05@gmail.com>
2026-02-05 12:15:34 +0530

    Add program to output messages in two lines

---
+++ C Basic/output in two lines using \n.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a==1){
+          printf("Hello\nWorld");
+    }
+    else if(a==2){
+          printf("Good\nMorning");
+    }
+    else{
+    printf("Invalid Input");
+    }
+    return 0;
+}
