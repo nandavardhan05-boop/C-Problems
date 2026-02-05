4a72599b1ea775b136ae8d09c77b7ee772a22cf1
nandavardhan05-boop <nandavardhan05@gmail.com>
2026-02-05 14:08:39 +0530

    Create Assign value later and print.c

---
+++ C Basic/Assign value later and print.c
@@ -0,0 +1,23 @@
+ #include <stdio.h>
+
+int main() {
+    int x;  
+    x = 10;            
+    printf("%d\n", x); 
+    return 0;
+}
+
+
+
+
+
+
+
+ #include <stdio.h>
+
+int main() {
+    int x;  
+    x = 60;            
+    printf("%d\n", x); 
+    return 0;
+}
