b7a9a70c8597e4b34e1d5ba331e39ed9305c6434
nandavardhan05-boop <nandavardhan05@gmail.com>
2026-02-05 11:50:03 +0530

    Create  your name.c

---
+++ C Basic/your name.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+
+    if (a == 1) {
+        printf("Sivan");
+    } else if (a == 2) {
+        printf("Ravi");
+    } else {
+        printf("Invalid Input");
+    }
+
+    return 0;
+}
