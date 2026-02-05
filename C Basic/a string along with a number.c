61a8b28a28ccc626eeb05834aa9d93d04ed88f24
nandavardhan05-boop <nandavardhan05@gmail.com>
2026-02-05 12:12:23 +0530

    Implement input handling for age and count
    Handle user input and provide output based on specific integer values.

---
+++ C Basic/a string along with a number.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a==20){
+          printf("Age: %d");
+    }
+    else if(a==5){
+          printf("Count: %d");
+    }
+    else{
+    printf("Invalid Input");
+    }
+    return 0;
+}
