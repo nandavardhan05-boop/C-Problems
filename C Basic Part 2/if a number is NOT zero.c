03f4122eae8ddd7e68cf234e51f7e7c37ee78fe8
nandavardhan05-boop <nandavardhan05@gmail.com>
2026-02-06 13:18:42 +0530

    Create if a number is NOT zero.c

---
+++ C Basic Part 2/if a number is NOT zero.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a ;
+    scanf("%d",&a);
+     if(a==0)
+     {
+         printf("ZERO");
+     }
+     else 
+     {
+         printf("NOT ZERO");
+     }
+    return 0;
+}
