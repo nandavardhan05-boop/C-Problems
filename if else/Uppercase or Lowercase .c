0d48ad84f0e762b3a15d54e6b0c069a827b5f8c9
nandavardhan05-boop <nandavardhan05@gmail.com>
2026-02-09 13:50:56 +0530

    Create  Uppercase or Lowercase .c

---
+++ if else/Uppercase or Lowercase .c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    scanf("%c",&ch);
+    if(ch>=65&&ch<90)
+    {
+        printf("Uppercase");
+    }
+    else if(ch>=97&&ch<=122)
+    {
+        printf("Lowercase");
+    }
+    else 
+    {
+        printf("Invalid Input");
+    }
+    
+    return 0;
+}
