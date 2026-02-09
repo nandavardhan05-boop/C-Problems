a2bf644cb128e134714ad369b6385902e9c93a6a
nandavardhan05-boop <nandavardhan05@gmail.com>
2026-02-09 12:23:08 +0530

    Create Check 3-Digit Number .c

---
+++ if else/Check 3-Digit Number .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int num;
+    scanf("%d",&num);
+    if("num>=100&&num<=999")
+    {
+        printf("Three digit number ");
+    }
+    else 
+    {
+        printf("Invalid input");
+    }
+    
+    return 0;
+}
