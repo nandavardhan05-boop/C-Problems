40bee32a592c4721e66d8097edb4b0bd784f7655
nandavardhan05-boop <nandavardhan05@gmail.com>
2026-02-09 15:07:45 +0530

    Create Number Within Range .c

---
+++ if else/Number Within Range .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n>=10&&n<=20)
+    {
+        printf("Number is within range ");
+    }
+    else 
+    {
+        printf("Number is out of  range ");
+    }
+    
+    return 0;
+}
