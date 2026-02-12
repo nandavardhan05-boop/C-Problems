a8438eb98f4cf731e4b6facf725cfd47b68d62a5
nandavardhan05-boop <nandavardhan05@gmail.com>
2026-02-12 11:29:06 +0530

    Create Electricity Bill Late Fine.c

---
+++ switch/Electricity Bill Late Fine.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    switch(n){
+        case 1:
+        printf("₹%d",n*50);
+        break;
+        case 2:
+        printf("₹%d",n*50);
+        break;
+         case 3:
+        printf("₹%d",n*50);
+        break;
+         case 4:
+        printf("₹%d",n*50);
+        break;
+         case 5:
+        printf("₹%d",n*50);
+        break;
+        case 6:
+        printf("₹%d",n*100);
+        break;
+        case 7:
+        printf("₹%d",n*100);
+        break;
+        case 8:
+        printf("₹%d",n*100);
+        break;
+        case 9:
+        printf("₹%d",n*100);
+        break;
+        case 10:
+        printf("₹%d",n*100);
+        break;
+        default :
+        printf("₹%d",n*200);
+    }
+    return 0;
+}
