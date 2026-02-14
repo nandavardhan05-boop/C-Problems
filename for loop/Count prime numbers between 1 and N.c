5df457f4bd8db327debac1647b57efa6ceca9a44
nandavardhan05-boop <nandavardhan05@gmail.com>
2026-02-14 21:11:52 +0530

    Create Count prime numbers between 1 and N.c

---
+++ for loop/Count prime numbers between 1 and N.c
@@ -0,0 +1,25 @@
+ #include <stdio.h>
+
+int main() {
+    int n, i, j, count = 0, isPrime;
+
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i++) {
+        isPrime = 1;  
+        for (j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            count++;
+        }
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
