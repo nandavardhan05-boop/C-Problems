098f9031e4225ac67ab9b49840b993f7edde6fa0
nandavardhan05-boop <nandavardhan05@gmail.com>
2026-02-28 13:59:09 +0530

    Create Find total delay time and count deliveries delayed more than 30 minutes.c

---
+++ for loop/Find total delay time and count deliveries delayed more than 30 minutes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int N,sum=0,count=0;
+    for(int i=1;i<=n;i++)
+    {
+        scanf("%d",&N);
+        sum=sum+N;
+
+        if(N>30)
+        {
+            count++;
+
+        }
+    }
+    printf("Total Delay:%d\n",sum);
+    printf("Delay Deliveries:%d",count);
+    
+    return 0;
+}
