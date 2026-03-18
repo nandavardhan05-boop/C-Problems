6918ea82e84f1fcc2c95ee14e0d040361fcb8ce7
nandavardhan05-boop <nandavardhan05@gmail.com>
2026-03-18 22:14:03 +0530

    Create height from centimeters to feet and inches.c

---
+++ if else part 2/height from centimeters to feet and inches.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int cm;
+    double inches, feet;
+
+    scanf("%d", &cm);
+
+    inches = cm ;
+    feet = inches ;
+
+    printf("%.2f\n", inches);
+    printf("%.2f", feet);
+
+    return 0;
+}
