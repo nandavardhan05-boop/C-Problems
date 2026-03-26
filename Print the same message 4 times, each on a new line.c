7b501315a05bb4254caed336ed3abcbf868478dc
nandavardhan05-boop <nandavardhan05@gmail.com>
2026-03-26 20:29:41 +0530

    Create Print the same message 4 times, each on a new line.c

---
+++ Print the same message 4 times, each on a new line.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char msg[100];
+
+    fgets(msg, sizeof(msg), stdin);
+
+    printf("%s", msg);
+    printf("%s", msg);
+    printf("%s", msg);
+    printf("%s", msg);
+
+    return 0;
+}
