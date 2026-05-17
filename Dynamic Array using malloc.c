7daae131abf0eb1ae1f2e848e390cb922d44ca48
nandavardhan05-boop <nandavardhan05@gmail.com>
2026-05-18 00:11:18 +0530

    Implement dynamic array using malloc in C

---
+++ Dynamic Array using malloc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n = 5;
+
+    int *arr = (int*)malloc(n * sizeof(int));
+
+    for(int i=0;i<n;i++)
+        arr[i] = i+1;
+
+    for(int i=0;i<n;i++)
+        printf("%d ", arr[i]);
+
+    free(arr);
+}
