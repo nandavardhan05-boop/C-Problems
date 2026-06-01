581cbd891ffb8a1971206155bfdaeaf2e4a87fe7
GUDIPALLI NANDAVARDHAN <nandavardhan05@gmail.com>
2026-06-01 19:54:39 +0530

    Add program to copy string without using strcpy

---
+++ Copy String Without strcpy().c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char str1[]="Hello";
+    char str2[20];
+    int i=0;
+
+    while(str1[i] != '\0') {
+        str2[i]=str1[i];
+        i++;
+    }
+
+    str2[i]='\0';
+
+    printf("%s", str2);
+}
