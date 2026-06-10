c4e8bc296371fe30ae94b61c5da2ec98a74bcf90
GUDIPALLI NANDAVARDHAN <nandavardhan05@gmail.com>
2026-06-10 20:34:53 +0530

    Add function to reverse a number

---
+++ Function to Reverse a Number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int reverse(int n) {
+    int rev = 0;
+
+    while(n > 0) {
+        rev = rev * 10 + n % 10;
+        n /= 10;
+    }
+    return rev;
+}
+
+int main() {
+    printf("%d", reverse(1234));
+}
