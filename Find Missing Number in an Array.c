6b99d2db753eecd4cb7d0e63475ab297ed5f076a
GUDIPALLI NANDAVARDHAN <nandavardhan05@gmail.com>
2026-07-05 23:49:47 +0530

    Add program to find missing number in an array

---
+++ Find Missing Number in an Array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {1, 2, 3, 5};
+    int n = 5;
+    int sum = 0;
+
+    for(int i = 0; i < n - 1; i++)
+        sum += arr[i];
+
+    int total = n * (n + 1) / 2;
+
+    printf("Missing Number = %d", total - sum);
+
+    return 0;
+}
