0cab988102579e360bbf72791f01bc2e28bf97ca
GUDIPALLI NANDAVARDHAN <nandavardhan05@gmail.com>
2026-07-05 11:41:56 -0300

    Add function to check if a number is even or odd

---
+++ Function to Check Even or Odd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void checkEvenOdd(int n) {
+    if(n % 2 == 0)
+        printf("Even");
+    else
+        printf("Odd");
+}
+
+int main() {
+    checkEvenOdd(15);
+    return 0;
+}
